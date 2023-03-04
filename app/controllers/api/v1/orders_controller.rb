9e3233be43a94d5dd63b46a1fe15fb1cd0397fc3
daichi1991 <ks-pan@live.jp>
2023-03-04 13:21:56 +0900

    order#createの修正

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,17 +5,21 @@ class Api::V1::OrdersController < ApplicationController
 
   def create
     raise Forbidden unless Account.find(order_params[:account_id]).user == @current_user
-    raise BadRequest, '販売情報が現在非公開です' if Sale.find(order_params[:sale_id]).publish == false
-    order = Order.new(order_params)
-    if order.save
-      render json: order, status: 201 and return
-    else
-      render json: order.errors, status: 400 and return
+    orders = []
+    Order.transaction do
+      order_params[:sales].each do |sale|
+        order = Order.new(account_id: order_params[:account_id], sale_id: sale[:id])
+        order.save!
+        orders.push(order)
+      end
     end
+    render json: orders, status: 201 and return
+    rescue => e
+      render json: e, status: 400 and return
   end
 
   private
   def order_params
-    params.require(:order).permit(:account_id, :sale_id)
+    params.require(:order).permit(:account_id, sales:[:id])
   end
 end
